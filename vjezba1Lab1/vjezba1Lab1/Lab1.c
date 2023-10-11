a0adf2b75ac90acb1a0ead77d4c71428d2bf8437
Alojz84 <acolak00@fesb.hr>
2023-10-11 20:03:15 +0200

    Partly finished lab 1

--- vjezba1Lab1/vjezba1Lab1/Lab1.c
+++ vjezba1Lab1/vjezba1Lab1/Lab1.c
@@ -1,7 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX_SIZE (58)
+#define MAX_LINE (1024)
+#define FILE_ERROR_OPEN (-1)
 #include<stdio.h>
+#include<stdlib.h>
+
+typedef struct student {
+	char name[MAX_SIZE];
+	char surname[MAX_SIZE];
+	double points;
+}Student;
+
+/*int readnumberofeowsinfile()
+{
+
+}*/
 
 int main() {
-	printf("Hello world!\n");
+	int noRows=0;
+	FILE* filePointer = NULL;
+	char buffer[MAX_LINE] = { 0 };
 
-	return 0;
+	filePointer = fopen("LAB1.txt", "r");
+	if (!filePointer) {
+		printf("File is not open!\n");
+		return FILE_ERROR_OPEN;
+	}
+
+	while (!feof(filePointer))
+	{
+		fgets(buffer, MAX_LINE, filePointer);
+		noRows++;
+	}
+	printf("%d", noRows);
+
+	fclose(filePointer);
+
+	return EXIT_SUCCESS;
 }
